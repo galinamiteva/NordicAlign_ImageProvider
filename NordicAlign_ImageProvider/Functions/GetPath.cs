89a8dc972dd2f7440308001ec7c66755479b662b
Gala Miteva <galina.miteva@utb.ecutbildning.se>
2025-04-05 00:14:19 +0200

    add functions

--- NordicAlign_ImageProvider/Functions/GetPath.cs
+++ NordicAlign_ImageProvider/Functions/GetPath.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Data.Contexts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
--- NordicAlign_ImageProvider/Program.cs
+++ NordicAlign_ImageProvider/Program.cs
@@ -1,13 +1,30 @@
+using Azure.Storage.Blobs;
+using Data.Contexts;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NordicAlign_ImageProvider.Services;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+//var builder = FunctionsApplication.CreateBuilder(args);
 
-builder.ConfigureFunctionsWebApplication();
+//builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
 
-builder.Build().Run();
+//builder.Build().Run();
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SQLSERVER")));
+        services.AddScoped<BlobServiceClient>(x => new BlobServiceClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT")));
+        services.AddScoped<FileService>();
+    })
+    .Build();
+
+host.Run();
